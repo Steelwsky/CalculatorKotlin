f8fe938ea61b21890c3526775900166ba95a391a
steelwsky <zero.rise@yandex.ru>
2019-09-12 20:37:58 +1000

    123

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.Html
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,24 +16,31 @@ class MainActivity : AppCompatActivity() {
     var isNewState = true
     var isFirstNumber = true
 
+    // firstNumber - последнее вводимое в tvMain
+    // secondNumber - результат последних вычислений в tvMiniHelper
     var firstNumber: Int = 0
-    var SecondNumber = ""
+    var secondNumber: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        tvMain.text = "0"
 //        val buttonOne = findViewById<Button>(R.id.buttonOne)
 //        fabDivide.setOnClickListener(new vi)
     }
 
     fun onNumber(view: View) {
-        val button = view as Button
-        val numberString = button.text
-        if (isNewState) tvMain.setText("")
-        isNewState = false
+
+//        if (isFirstNumber) {
+//        val numberString = button.text
+//        if (isNewState) tvMain.setText("")
+//        isNewState = false
 //        if (isFirstNumber)
+        val button = view as Button
+        if (!isLastNumeric) {
+            tvMain.text = ""
+        }
         tvMain.append((button).text)
         isLastNumeric = true
     }
@@ -41,22 +49,90 @@ class MainActivity : AppCompatActivity() {
         if (isLastNumeric && !isLastDP) {
             tvMain.append((view as Button).text)
             isLastDP = true
+            isLastNumeric = false
         }
     }
 
+    fun onClear(view: View) {
+        tvMain.text = "0"
+//        tvMiniHelper.text = ""
+        isFirstNumber = true
+        isLastDP = false
+//        isNewState = true
+        firstNumber = 0
+        secondNumber = 0
+    }
+
+    //   TODO: отделить условие when другой функцией и добавить ее выполнение здесь
     fun onOperator(view: View) {
-        tvMiniHelper.text = tvMain.text
-        firstNumber = tvMain.text.toString().toInt()
-        tvMain.text = ""
+//        val button = view as Button
+        var operator = ""
+        if (isLastNumeric) {
+            isLastNumeric = false
+            if (isFirstNumber) {
+//                tvMiniHelper.text = tvMain.text
+                firstNumber = tvMain.text.toString().toInt()
+                isFirstNumber = false
+            } else {
+                secondNumber = tvMain.text.toString().toInt()
+                whenHelper(view, secondNumber)
 
+                // tut fun onEqual
+//                operator = button.text.toString()
+            }
+        } else return
 
     }
 
-    fun saveNumber () {
+
+    fun saveNumber() {
 
     }
 
+    fun onEqual(view: View) {
+
+    }
+
+    fun whenHelper(v: View, sec: Int) {
+        var operator = ""
+        when (v) {
+            fabPlus -> operator = "+"
+            fabEquals -> operator = "="
+
+        }
+        if (operator.equals("+")) {
+            firstNumber += sec
+            tvMain.text = firstNumber.toString()
+        }
+
+    }
 
 }
 
 
+//fun onOperator(view: View) {
+//    var operator = view
+//        when (view) {
+//            fabPlus -> operator = "+"
+//            fabDivide -> operator = "/"
+//
+//    if (isLastNumeric) {
+//        tvMiniHelper.text = tvMain.text
+//        if (isFirstNumber) {
+//            firstNumber = tvMain.text.toString().toInt()
+//            isFirstNumber = false
+//        } else {
+//
+//        }
+//        if (operator.equals("+")) secondNumber += firstNumber
+//        if (operator.equals("/")) secondNumber /= firstNumber
+//
+//        isFirstNumber = false
+//        tvMain.text = ""
+//        isLastNumeric = false
+//
+//    } else {
+//        return
+//
+//    }
+//}
